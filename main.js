3a35a7539bfb62a0157f9416ca71b3b6e065bbe5
Sean M <53903852+shmccoy@users.noreply.github.com>
2019-12-03 17:48:12 -0500

    edit 2

--- main.js
+++ main.js
@@ -1,33 +1,44 @@
+function watchSubmit() {
+    console.log("123");
+    $("#titleForm").submit(
+
+        function(event) {
+            event.preventDefault()
+            console.log(test);
+        }
+    )
+}
+$(watchSubmit)
 fetch('http://omdbapi.com/?apikey=7b56d36&t=it')
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            }
-            throw new Error(response.statusText);
-        })
-        .then(movie => console.log(movie));
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error(response.statusText);
+    })
+    .then(movie => console.log(movie));
+
 
 function openPage(pageName, elmnt, color) {
-  // Hide all elements with class="tabcontent" by default */
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
+    // Hide all elements with class="tabcontent" by default
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
 
-  // Remove the background color of all tablinks/buttons
-  tablinks = document.getElementsByClassName("tablink");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].style.backgroundColor = "";
-  }
+    // Remove the background color of all tablinks/buttons
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+    }
 
-  // Show the specific tab content
-  document.getElementById(pageName).style.display = "block";
+    // Show the specific tab content
+    document.getElementById(pageName).style.display = "block";
 
-  // Add the specific color to the button used to open the tab content
-  elmnt.style.backgroundColor = color;
+    // Add the specific color to the button used to open the tab content
+    elmnt.style.backgroundColor = color;
 }
 
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
-
