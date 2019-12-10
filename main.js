a2ef8fdcc9ddece4699d3567c11aa25d930b7776
Sean M <53903852+shmccoy@users.noreply.github.com>
2019-12-10 18:45:37 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,52 @@
-function watchSubmit() {
-    console.log("123");
-    $("#titleForm").submit(
+function watchSubmit(event) {
+    event.preventDefault();
+    //get the value
+    const searchBar = $("#titleForm").val();
+    performApiSearch(searchBar);
+  console.log('test');
+  }
 
-        function(event) {
-            event.preventDefault()
-            console.log(test);
+
+function performApiSearch(query) {
+    const base_url = "https://api.themoviedb.org/3/search/movie?api_key=625e6305ec06081d9670035238e43576&language=en-US";
+    const url = `${base_url}&query=${query}`;
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(response.statusText);
         }
-    )
-}
-$(watchSubmit)
-fetch('http://omdbapi.com/?apikey=7b56d36&t=it')
-    .then(response => {
-        if (response.ok) {
-            return response.json();
-        }
-        throw new Error(response.statusText);
-    })
-    .then(movie => console.log(movie));
+        return response.json();
+      })
+      .then(data => render(data.results))
+      .catch(error => {
+        $(".result").html(error.message);
+      });
+  console.log('test2');
+  }
+
+  function render(results) {
+      
+    $(".result").html(
+      results
+        .map(
+          item => `<div class="image">
+      <img src= "https://image.tmdb.org/t/p/w500${results.poster_path[0]}"/>
+      <div class="content">
+      <h2>${results.title[0]}</h2>
+      <p>${results.overview[0]}</p>
+      </div>
+    </div>`
+        )
+        .join("")
+    );
+    console.log('test3');
+  }
+
+  function main() {
+    $("#titleForm").submit(watchSubmit);
+  }
+  //the jQuery ready function
+  $(main);
 
 
 function openPage(pageName, elmnt, color) {
