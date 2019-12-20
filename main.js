73ca465c2ea60535df889b0dfa2d72b566fd2bae
Sean M <53903852+shmccoy@users.noreply.github.com>
2019-12-19 19:32:28 -0500

    edit

--- main.js
+++ main.js
@@ -1,44 +1,154 @@
-function watchSubmit() {
-    console.log("123");
-    $("#titleForm").submit(
-
-        function(event) {
-            event.preventDefault()
-            console.log(test);
-        }
-    )
+function watchMovieSubmit(event) {
+  event.preventDefault();
+  //get the value
+  const searchBarMovie = $("#titleForm").val();
+  performMovieSearch(searchBarMovie);
+  console.log(searchBarMovie);
+  console.log("test0");
 }
-$(watchSubmit)
-fetch('http://omdbapi.com/?apikey=7b56d36&t=it')
+
+function watchActorSubmit(event) {
+  event.preventDefault();
+  const searchBarActor = $("#actorForm").val();
+  performActorSearch(searchBarActor);
+
+}
+
+function newReleases() {
+  const url =
+    "https://api.themoviedb.org/3/movie/now_playing?api_key=625e6305ec06081d9670035238e43576&language=en-US";
+  fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      const movies = data.results;
+      const sortedMovies = movies.sort(sortCriteria).slice(0, 5);
+      console.log(sortedMovies);
+    });
+}
+​
+function sortCriteria(a, b) {
+  if (moment(b.release_date).isAfter(a.release_date)) {
+    return 1;
+  } else {
+    if (moment(b.release_date).isSame(a.release_date)) {
+      return 0;
+    } else {
+      return -1;
+    }
+  }
+}
+​
+function performMovieSearch(query) {
+  const base_url =
+    "https://api.themoviedb.org/3/search/movie?api_key=625e6305ec06081d9670035238e43576&language=en-US";
+  const url = `${base_url}&query=${query}`;
+  fetch(url)
     .then(response => {
-        if (response.ok) {
-            return response.json();
-        }
+      if (!response.ok) {
         throw new Error(response.statusText);
+      }
+      return response.json();
     })
-    .then(movie => console.log(movie));
-
-
-function openPage(pageName, elmnt, color) {
-    // Hide all elements with class="tabcontent" by default
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-
-    // Remove the background color of all tablinks/buttons
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].style.backgroundColor = "";
-    }
-
-    // Show the specific tab content
-    document.getElementById(pageName).style.display = "block";
-
-    // Add the specific color to the button used to open the tab content
-    elmnt.style.backgroundColor = color;
+    .then(data => renderMovie(data.results))
+    .catch(error => {
+      $(".result").html(error.message);
+    });
+  console.log("test1");
 }
+​
+function performActorSearch(query) {
+  const base_url =
+    "https://api.themoviedb.org/3/search/person?api_key=625e6305ec06081d9670035238e43576&language=en-US";
+  const url = `${base_url}&query=${query}`;
+  fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
+    })
+    .then(data => renderActor(data.results))
+    .catch(error => {
+      $("#actor").html(error.message);
+    });
+  console.log("test2");
+}
+​
+function renderNewReleases(results) {
+  console.log(results);
+  $("#newreleases").html(
+    results
+      .map(
+        item => `<div class="image">
+      <img src= "https://image.tmdb.org/t/p/w500${item.poster_path}"/>
+      <div class="content">
+      <h2>${item.title}</h2>
+      <p>${item.overview}</p>
+      </div>
+    </div>`
+      )
+      .join("")
+  );
+}
+​
+function renderMovie(results) {
+  console.log(results);
+  $("#movie").html(
+    results
+      .map(
+        item => `<div class="image">
+      <img src= "https://image.tmdb.org/t/p/w500${item.poster_path}"/>
+      <div class="content">
+      <h2>${item.title}</h2>
+      <p>${item.overview}</p>
+      </div>
+    </div>`
+      )
+      .join("")
+  );
+  console.log("test3");
+}
+​
+function renderActor(results) {
+  console.log(results);
+  $("#actor").html(
+    results
+      .map(
+        item => `<div class="image">
+      <img src= "https://image.tmdb.org/t/p/w500${item.known_for[0].poster_path}"/>
+      <div class="content">
+      <h2>${item.known_for[0].title}</h2>
+      <p>${item.known_for[0].overview}</p>
+      </div>
+    </div>`
+      )
+      .join("")
+  );
+  console.log("test4");
+}
+​
+function main() {
+  console.log("The main function");
+  $(".searchBarMovie").submit(watchMovieSubmit);
+  $(".searchBarActor").submit(watchActorSubmit);
+  newReleases()
 
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
+​
+  $(".tablink").click(tabLinkHandler);
+​
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
+}
+//the jQuery ready function
+$(main);
+​
+function tabLinkHandler(e) {
+  const btn = $(e.target).data("tab");
+  console.log(btn);
+​
+  $(".tabcontent").hide();
+  $(".tablink").removeClass("selectedTab");
+  $(e.target).addClass("selectedTab");
+​
+  $(`#${btn}`).show();
+}
