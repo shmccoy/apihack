bba3aa0ab4fc67630c1b0da24dfb1e22b4eee7f8
Sean M <53903852+shmccoy@users.noreply.github.com>
2019-12-19 19:39:10 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function watchMovieSubmit(event) {
   event.preventDefault();
   //get the value
@@ -7,66 +6,12 @@ function watchMovieSubmit(event) {
   console.log(searchBarMovie);
   console.log("test0");
 }
-=======
-function watchSubmit(event) {
-    event.preventDefault();
-    //get the value
-    const searchBar = $("#titleForm").val();
-    performApiSearch(searchBar);
-  console.log('test');
-  }
-
-
-function performApiSearch(query) {
-    const base_url = "https://api.themoviedb.org/3/search/movie?api_key=625e6305ec06081d9670035238e43576&language=en-US";
-    const url = `${base_url}&query=${query}`;
-    fetch(url)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(response.statusText);
-        }
-        return response.json();
-      })
-      .then(data => render(data.results))
-      .catch(error => {
-        $(".result").html(error.message);
-      });
-  console.log('test2');
-  }
-
-  function render(results) {
-      
-    $(".result").html(
-      results
-        .map(
-          item => `<div class="image">
-      <img src= "https://image.tmdb.org/t/p/w500${results.poster_path[0]}"/>
-      <div class="content">
-      <h2>${results.title[0]}</h2>
-      <p>${results.overview[0]}</p>
-      </div>
-    </div>`
-        )
-        .join("")
-    );
-    console.log('test3');
-  }
-
-  function main() {
-    $("#titleForm").submit(watchSubmit);
-    // Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
-
-  }
-  //the jQuery ready function
-  $(main);
->>>>>>> 97d253edae4d2e90fba146200965f82bf47a1af8
 
 function watchActorSubmit(event) {
   event.preventDefault();
   const searchBarActor = $("#actorForm").val();
+  performMovieSearch(searchBarMovie);
   performActorSearch(searchBarActor);
-
 }
 
 function newReleases() {
@@ -80,7 +25,7 @@ function newReleases() {
       console.log(sortedMovies);
     });
 }
-​
+
 function sortCriteria(a, b) {
   if (moment(b.release_date).isAfter(a.release_date)) {
     return 1;
@@ -92,7 +37,7 @@ function sortCriteria(a, b) {
     }
   }
 }
-​
+
 function performMovieSearch(query) {
   const base_url =
     "https://api.themoviedb.org/3/search/movie?api_key=625e6305ec06081d9670035238e43576&language=en-US";
@@ -110,7 +55,7 @@ function performMovieSearch(query) {
     });
   console.log("test1");
 }
-​
+
 function performActorSearch(query) {
   const base_url =
     "https://api.themoviedb.org/3/search/person?api_key=625e6305ec06081d9670035238e43576&language=en-US";
@@ -128,7 +73,7 @@ function performActorSearch(query) {
     });
   console.log("test2");
 }
-​
+
 function renderNewReleases(results) {
   console.log(results);
   $("#newreleases").html(
@@ -145,7 +90,7 @@ function renderNewReleases(results) {
       .join("")
   );
 }
-​
+
 function renderMovie(results) {
   console.log(results);
   $("#movie").html(
@@ -163,7 +108,7 @@ function renderMovie(results) {
   );
   console.log("test3");
 }
-​
+
 function renderActor(results) {
   console.log(results);
   $("#actor").html(
@@ -181,32 +126,27 @@ function renderActor(results) {
   );
   console.log("test4");
 }
-​
+
 function main() {
   console.log("The main function");
   $(".searchBarMovie").submit(watchMovieSubmit);
   $(".searchBarActor").submit(watchActorSubmit);
-  newReleases()
 
-<<<<<<< HEAD
-​
   $(".tablink").click(tabLinkHandler);
-​
+
   // Get the element with id="defaultOpen" and click on it
   document.getElementById("defaultOpen").click();
 }
 //the jQuery ready function
 $(main);
-​
+
 function tabLinkHandler(e) {
   const btn = $(e.target).data("tab");
   console.log(btn);
-​
+
   $(".tabcontent").hide();
   $(".tablink").removeClass("selectedTab");
   $(e.target).addClass("selectedTab");
-​
+
   $(`#${btn}`).show();
 }
-=======
->>>>>>> 97d253edae4d2e90fba146200965f82bf47a1af8
